25d22790c9646ea5c71896d5decc07234d92c5af
Platte Clark <plattinum95@gmail.com>
2023-05-22 22:51:58 -0600

    The titleScreen update

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Button playButton;
+    public Button loadButton;
+    public Button settingsButton;
+    public Button exitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(OnPlayButtonClick);
+        loadButton.onClick.AddListener(OnLoadButtonClick);
+        settingsButton.onClick.AddListener(OnSettingsButtonClick);
+        exitButton.onClick.AddListener(OnExitButtonClick);
+    }
+
+    private void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene("PlayGround");
+    }
+
+    private void OnLoadButtonClick()
+    {
+        // Load game somehow
+    }
+
+    private void OnSettingsButtonClick()
+    {
+        //
+    }
+
+    private void OnExitButtonClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
